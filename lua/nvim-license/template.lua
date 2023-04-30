480ab297ca05d3b0c0d7181f4dd0306b216cc4ee
Samir Bioud <samir@kronsy.dev>
2023-04-30 15:28:43 +0100

    Bugfix: Added error check for file opening

--- lua/nvim-license/template.lua
+++ lua/nvim-license/template.lua
@@ -53,6 +53,10 @@ end
 
 local function open_and_substitute(path, options)
   local f = io.open(path, "r")
+  if f == nil then
+    vim.api.nvim_err_writeln("Failed to read path: " .. path)
+    return nil
+  end
   local data = f:read("a")
   data = data:gsub("{{ year }}", options["year"]):gsub("{{ organization }}", options["author"]):gsub("{{ project }}", options["project"])
   f:close()
